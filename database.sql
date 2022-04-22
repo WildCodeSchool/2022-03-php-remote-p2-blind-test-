fc26e59f41377b6529d83a29a1890c9b3cee8a29
Mickael Desbiens <desbiens.mickael01@gmail.com>
2022-04-22 23:42:30 +0200

    fix css and transfer the tracks folder to the uploads folder

--- database.sql
+++ database.sql
@@ -21,43 +21,6 @@ SET time_zone = "+00:00";
 --
 
 -- --------------------------------------------------------
-
---
--- Structure de la table `item`
---
-
-CREATE TABLE `item` (
-  `id` int(11) UNSIGNED NOT NULL,
-  `title` varchar(255) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
---
--- Contenu de la table `item`
---
-
-INSERT INTO `item` (`id`, `title`) VALUES
-(1, 'Stuff'),
-(2, 'Doodads');
-
---
--- Index pour les tables exportées
---
-
---
--- Index pour la table `item`
---
-ALTER TABLE `item`
-  ADD PRIMARY KEY (`id`);
-
---
--- AUTO_INCREMENT pour les tables exportées
---
-
---
--- AUTO_INCREMENT pour la table `item`
---
-ALTER TABLE `item`
-  MODIFY `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
@@ -76,10 +39,7 @@ CREATE TABLE `track` (
     PRIMARY KEY(`id`)
     ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 --
--- Foreign Key Constrait pour la table track :
---
-ALTER TABLE `track`
-ADD CONSTRAINT fk_track_category FOREIGN KEY (category_id) REFERENCES category (id);
+
 --
 -- Contenu de la table `track`
 --
@@ -137,3 +97,8 @@ INSERT INTO `user` (`id`, `nickname`) VALUES
 (1, 'Trombone');
 -- Structure de la table `user`
 --
+
+-- Foreign Key Constrait pour la table track :
+--
+ALTER TABLE `track`
+ADD CONSTRAINT fk_track_category FOREIGN KEY (category_id) REFERENCES category (id);
