0a2e6378d82c0362e205dee5c6ece51df686a398
Jean-François Morin <jeanfrancois.morin@wildcodeschool.com>
2022-05-05 15:47:01 +0200

    fix deploy & DB time zone

--- database.sql
+++ database.sql
@@ -8,7 +8,7 @@
 -- Version de PHP :  7.0.22-0ubuntu0.16.04.1
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET time_zone = "+00:00";
+SET GLOBAL time_zone = "+02:00";
 
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
